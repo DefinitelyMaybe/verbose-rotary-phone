60234755d26a8e0eb0d1d9d70194207aa8693741
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-07-21 21:12:28 +1200

    init sveltekit commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+	<title>Welcome</title>
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button on:click="{handleClick}">click me</button>
+
+<script>
+  let x = 1
+
+  function handleClick() {
+    console.log(x);
+    x += 1
+  }
+</script>
