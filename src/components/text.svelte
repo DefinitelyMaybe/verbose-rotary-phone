68d5d6fb02432a3967a05d611e9fc0f4cdc6929f
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-07-29 21:18:12 +1200

    add text object

---
+++ src/components/text.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Object from "./object.svelte";
+  let text = "Text placeholder"
+</script>
+
+<Object>
+  <textarea bind:value={text}></textarea>
+</Object>
+
+<style>
+  textarea {
+    font-size: x-large;
+    /* color: whitesmoke;
+    background-color: black; */
+  }
+</style>
