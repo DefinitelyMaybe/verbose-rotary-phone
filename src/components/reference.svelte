b8b1e85d8c1b58e045168c3c6d514806e6e37d9a
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-07-29 21:18:04 +1200

    add ref object

---
+++ src/components/reference.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Object from "../components/object.svelte";
+  // import { onMount } from "svelte";
+  let ref = ""
+  // let thing
+
+  // onMount( () => {
+  //   console.log(ref);
+  //   // ref = URL.createObjectURL(thing)
+  //   console.log(thing);
+  // })
+</script>
+
+<Object>
+  {#if ref}
+    <a href="{ref}">{ref}</a>
+  {:else}
+    <p>Reference-Placeholder</p>
+  {/if}
+</Object>
