629e29627a7f3fabad6e0e5f033feccfdc37af77
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2021-07-29 21:31:38 +1200

    add type declaration

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="@sveltejs/kit" />
+
+declare namespace svelte.JSX {
+  interface HTMLAttributes<T> {
+      // If you want to use on:beforeinstallprompt
+      // onbeforeinstallprompt?: (event: any) => any; // any;
+      // If you want to use myCustomAttribute={..} (note: all lowercase)
+      onpanmove?: () => void;
+      // You can replace any with something more specific if you like
+  }
+}
